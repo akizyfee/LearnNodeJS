46ca8779f76abb43e3590b60ab5f2692cede8553
akizyfee <87452790+akizyfee@users.noreply.github.com>
2022-06-18 18:53:36 +0800

    add postman環境變數&swagger網址

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Meta API',
         descripttion: '生成的範例文件'
     },
-    host: 'localhost:3000',
+    host: 'https://fathomless-ravine-88500.herokuapp.com/',
     schemas: ['http', 'https'],
     securityDefinitions: {
         apiKeyAuth: {
